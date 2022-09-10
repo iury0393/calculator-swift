6ee506a06c97ee79bc781ed9c8dac330851d62bc
Iury Vasconcelos <iury0393@gmail.com>
2022-09-10 11:22:42 -0300

    Subindo layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
     }
 
-
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
 }
 
